265561f8b0f26cb9ea2314daca19034e021c3461
Kartik Gulpadia <50989866+KartikTG02@users.noreply.github.com>
2026-05-08 04:08:25 +0000

    Updated dashboard

--- init_kafka.py
+++ init_kafka.py
@@ -1,10 +1,7 @@
 from confluent_kafka.admin import AdminClient, NewTopic
 
-# 1. Connect to your local Kafka Broker
 admin = AdminClient({'bootstrap.servers': 'localhost:9092'})
 
-# 2. Define the exact specifications of your topic
-# 1 partition is fine for local dev. Replication factor 1 because you have 1 broker.
 topic_name = "nse_live_ticks"
 new_topic = NewTopic(topic_name, num_partitions=1, replication_factor=1)
 
@@ -16,8 +13,7 @@ futures = admin.create_topics([new_topic])
 # 4. Check the results
 for topic, future in futures.items():
     try:
-        future.result()  # This blocks until the creation succeeds or fails
+        future.result() 
         print(f"✅ Success! Topic '{topic}' is permanently registered in Kafka.")
     except Exception as e:
-        # If it already exists, it will throw an error, which is perfectly fine
         print(f"Notice for '{topic}': {e}")
